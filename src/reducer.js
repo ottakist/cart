9f1719f83b129ff3894c8565deb3e07d6f594781
ottakist <102596775+ottakist@users.noreply.github.com>
2022-09-02 23:52:53 +0300

    reducer commit

---
+++ src/reducer.js
@@ -0,0 +1,32 @@
+import CartItem from "./CartItem"
+
+const reducer =(state,action)=>{
+ if(action.type==='CLEAR_CART'){
+  return{...state,cart:[]}
+ }
+ if(action.type === 'REMOVE'){
+  return{...state,cart: state.cart.filter((CartItem)=>
+   CartItem.id !== action.payload
+  )}
+ }
+ if(action.type === 'INCREASE'){
+  let tempCart = state.cart.map((CartItem)=>{
+   if(CartItem.id === action.payload){
+    return {...CartItem,amount:CartItem.amount+1}
+   }
+   return CartItem
+  })
+  return {...state,cart:tempCart}
+ }
+ if(action.type === 'DECREASE'){
+let tempCart = state.cart.map((CartItem) => {
+  if (CartItem.id === action.payload) {
+    return { ...CartItem, amount: CartItem.amount -1 };
+  }
+  return CartItem;
+}).filter((CartItem)=> CartItem.amount !== 0);
+return { ...state, cart: tempCart };
+ }
+  return state
+}
+export default reducer
